ff6852e8a8f07b1a70f9744e3bed9d3c823e2bc6
sangxyz <ndsangxyz@gmail.com>
2020-11-18 09:04:34 +0700

    add JUnit code $ .jar(s)

--- src/main.java
+++ src/main.java
@@ -27,12 +27,34 @@ public class main {
         //?lam app du an code
         //2 lam tren file git ignore danh dau len xuong
         //cach 2 tu dong lam qua api giang ho cho san
-//        https://www.toptal.com/developers/gitignore
-//.gitignore
-//all txt
-//vao bin
-//neu e 2 cham
-//bua sau hoc readme
-System.out.println("chuc cuoi tuan vv");
+        //        https://www.toptal.com/developers/gitignore
+        //.gitignore
+        //all txt
+        //vao bin
+        //neu e 2 cham
+        //bua sau hoc readme
+        System.out.println("chuc cuoi tuan vv");
+        //MINH MUON KET LUAN MINH NGON, MINH OHAI TEST CA C TRUONG HOP XAI HAM
+        //TRUONG HOP 5! 0! 
+        //minh phai thiet ke cac loai testcase tinh huong test sau do minh verify
+        //giua cac ham tra ve cua ham actual co khop voi expested ca minh du doan ko
+        //ham chay ohai tra ve
+        //neu moi thu khop, tuc la actual == expedted minh moi dam noi HAM NGON
+        //CACH NAY co nhuoc diem nhin bang mat so sanh ket qua cua tng case tinh  huong test
+        // cach nay on nhung tiem an sai sot do minh nhin
+        //CACH NGAN CAo
+        //cung nhin= mat nhing chi nhin 2mau xanh do cho moi test case
+        //co nghia neu ban sai ham voii 100 tham so chay ham 100 tinh huong khac nhau
+        //ban chinhin thay duy nhat 2 ham xanh do
+        //moi test case phai co expected ==actual thi moi xanh
+        //DO, moi mau xanh chi can it nhat thang do ket luan dam do
+        //chi co 1 tinh huong sai ko khop ket luan luon, ham hong on dinh
+        // ai dam sai ham ko on dinh
+        //muon lam dc dieu nay, can su phu giup cua cac thu vien ben ngoai JDK, goi la unit test framwork
+        //cac bo unit test noi tieng: JUNIT, TESTNG, NUnit, XUNIT, PHPUNIT
+        //bua nay minh hoc xanh do qua Junit
+        //van vs muc tieu test ham co ngon ko nhng chi nhin dung 2 mau
+        //tets cac tinh huong chay ham
+
     }
 }
---
+++ test/MathUtilityTEST.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import sangnd.util.MathUtility;
+//import static sangnd.util.MathUtility;
+//chi tu jDK 5? ham sattic goi ma hok can ten class cham
+//do minh da choi tro khai bao van can san ten class va ten ham static qua lenh
+//import static
+//sau nay xai ham static cua class nay ko can ten class cham sd nhu c
+
+
+/**
+ *
+ * @author nds72
+ */
+public class MathUtilityTEST {
+
+    //viet ra nhung vc nhung tinh huong sai ham getFactoria()
+    //kiem tra xem actual == expected nhu truyen thonh gay ko
+    //nhung o day co xin so hon ko ngoai vs xanh do
+    //ngoai ra ra con dung cho CI -- countinous Integration
+    // CD countinous Delivery
+    // CT countinous Regrewssion TEST
+    // DEvOPs== Developmet and operation
+    //app lien tuc dc kiem tra chat luong code bulid ra file jar thanh war day len mot server trung gian Stafing evn de cho nhom test san sang test
+    //sau do co the day len production server may chay that noi khach hang
+    //moi vc se bat dau ke tu luc devlopber push code len github va moi thu cu the thu dong ko can cang thiep con nguoi
+    @Test //tu khoa nay tuong duong public static void main
+    //trong ham nay minh se show ra mau xanh do dua tren su khop nhau cua EXPESTED VS ACTUAL qua ham aseertEquals().. ma thu vien cung cap
+    //assert () du cac loai assert tren cuoc doi  -asser (v) so sah
+    //thay vi so sanh - mat ta nho ham so sanh gium, thay mau gium ta
+    public void getFactorial_RunsWell_IfValidArgument() {
+        assertEquals(120,MathUtility.getFactorial(5));//hay tinh gium tui 5 ! coi no co la 120 hem
+        //co thi xanh
+        //ko bao do
+        assertEquals(24,MathUtility.getFactorial(5));
+        //ky thuat tdd
+    }
+}
